07606870382f065cd9622990640187e32f6ed4d5
inha <dltjgus0709@inha.edu>
2024-10-12 18:43:19 +0900

    6주차

--- routes/kakao.js
+++ routes/kakao.js
@@ -6,6 +6,20 @@ router.get('/book', function(req, res, next) {
   res.render('index', { title: '도서검색', pageName:'kakao/book.ejs' });
 });
 
+// 도서 상세 정보
+router.get('/book/info', function(req, res) {
+  const title = req.query.title
+  const thumbnail = req.query.thumbnail
+  const price = req.query.price
+  const authors = req.query.authors
+  const publisher = req.query.publisher
+  const isbn = req.query.isbn
+  const contents = req.query.contents
+  const info = {title, thumbnail, price, authors, publisher, isbn, contents}
+  console.log('info', info)
+  res.render('index', { title: '도서정보', pageName:'kakao/info.ejs', info});
+});
+
 // 지역 검색
 router.get('/local', function(req, res, next) {
   res.render('index', { title: '지역검색', pageName:'kakao/local.ejs' });
